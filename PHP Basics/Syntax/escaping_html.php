744714c61256f5581702617cd8099ed31b403cae
Saiful <msisaifulsaif@gmail.com>
2019-09-03 23:02:53 +0600

    php basic tags and comment

---
+++ PHP Basics/Syntax/escaping_html.php
@@ -0,0 +1,10 @@
+<?php if (true): ?>
+  This will show if an expression is true.
+<?php else: ?>
+  Otherwise this will show.
+<?php endif; ?>
+
+<?php echo "Hello PHP\n";
+  echo 'Hello world\n'
+//Don't need to add semicolon before closing tag   ?>
+<?= 'Hello World with short echo tag' ?>
---
+++ PHP Basics/Syntax/tags_and_comment.php
@@ -0,0 +1,18 @@
+<%, //Deprecated tag %>
+<script language="php"> //Deprecated tag </script>
+
+<?php //PHP standard tag  ?>
+<? //Short tag. Need to enable short_open_tag in php.ini file?>
+<?php
+  //Not need to add a closing tag pure PHP file 
+?>
+
+<?php
+    echo 'This is a test'; // This is a one-line c++ style comment
+    /* This is a multi line comment
+       yet another line of comment */
+    echo 'This is yet another test';
+    echo 'One Final Test'; # This is a one-line shell-style comment
+?>
+
+
